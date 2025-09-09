3fffef10ee4a62f27eca948a32d1039676a5da43
professor Fernando <fernandoleonid@gmail.com>
2025-09-09 20:07:58 -0300

    exemplo03

---
+++ live01/exemplo03.py
@@ -0,0 +1,10 @@
+numero_extenso = input("Digite um número por extenso: ")
+
+if numero_extenso == "um":
+    numero = 1
+elif numero_extenso == "dois":
+    numero = 2
+elif numero_extenso == "cinco":
+    numero  = 5
+
+print (f"O número digita é: {numero}") 
