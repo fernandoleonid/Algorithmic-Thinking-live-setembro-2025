76e472e13f834b5cd28e9811e436e3f632d6a005
professor Fernando <fernandoleonid@gmail.com>
2025-09-09 20:25:16 -0300

    exemplo 05

---
+++ live01/exemplo05.py
@@ -0,0 +1,7 @@
+senha_cadastrada = "123"
+senha_digitada = input ("Digite sua senha: ")
+
+while senha_digitada != senha_cadastrada:
+    senha_digitada = input ("Digite sua senha: ")
+
+print ("Entrando no sistema...")
