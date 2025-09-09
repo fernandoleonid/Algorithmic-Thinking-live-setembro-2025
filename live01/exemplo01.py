276c103ec734420d88bf64dac0fb36d17b5f1c22
professor Fernando <fernandoleonid@gmail.com>
2025-09-09 19:42:33 -0300

    exemplo 01

---
+++ live01/exemplo01.py
@@ -0,0 +1,10 @@
+# Váriavel string
+nome = "José da Silva"
+idade = "20"
+altura = 1.80
+presente = True
+# transformando string em inteiro
+ano_nascimento = 2025 - int(idade)
+
+# concatenando com f-string
+print (f"{nome} tem {idade} anos e {altura} de altura")
