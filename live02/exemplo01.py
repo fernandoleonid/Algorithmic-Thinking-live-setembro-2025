ab44405b36ac0eb1773dcedb87247f7f49c0befe
professor Fernando <fernandoleonid@gmail.com>
2025-09-16 19:29:54 -0300

    Exemplo 1 e 2 da live 2

---
+++ live02/exemplo01.py
@@ -0,0 +1,15 @@
+numeros = [3, 67, 89, 3, 455]
+
+print ("teste")
+
+numeros.append(100)
+
+print (numeros)
+
+numeros.insert(3,333)
+
+print (numeros)
+
+numeros.pop(2)
+
+print(numeros)
---
+++ live02/exemplo02.py
@@ -0,0 +1,8 @@
+cliente = {
+    "nome": "Ana Julia",
+    "idade": 28,
+    "nota" : 7,
+    "celular": "119717182828"
+}
+
+print (cliente["nome"])
